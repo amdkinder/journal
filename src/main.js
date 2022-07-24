2285f648b60e4f9d3a9e34be096c5d19bee3b0bf
kuchkaroff <akhrorbek.kuchkarov@gmail.com>
2022-07-24 17:08:58 +0500

    parallax removed

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuesax from 'vuesax'
-import Parallax from 'vue-parallaxy'
 Vue.config.productionTip = false
 
 import 'vuesax/dist/vuesax.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 Vue.use(Vuesax, {})
-Vue.use(Parallax)
 
 new Vue({
   router,
